f6eda51b4f3b170f8c99404c4a57256e49885495
Andrew Pinon <andrewapinon@gmail.com>
2020-04-27 12:03:53 +0800

    added burger menu

--- js/index.js
+++ js/index.js
@@ -1,2 +1,14 @@
-import {gsap} from 'gsap';
+const burgerBtn = document.getElementById("burger-btn");
+const mainContainer = document.querySelector(".mainContainer");
+const section1 = document.querySelector(".section1");
+// const filter = document.querySelector(".filter");
+
+burgerBtn.addEventListener("click",()=>{
+    gsap.to("#phoneNavBar",{x:"-100%"})
+    gsap.to(".filter",{opacity:0,display:"none"})
+    section1.addEventListener("click",()=>{
+        gsap.to("#phoneNavBar",{x:"100%"})
+        gsap.to(".filter",{opacity:1,display:"block"})
+    })
+})
 
