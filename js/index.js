cb5b0aa5c6a6768109854ff07b069aa60397700b
Andrew <andrewapinon@gmail.com>
2020-05-08 14:02:19 +0800

    added section 3 4 5 6 7

--- js/index.js
+++ js/index.js
@@ -2,27 +2,31 @@ const burgerBtn = document.getElementById("burger-btn");
 const mainContainer = document.querySelector(".mainContainer");
 const section1 = document.querySelector(".section1");
 const section2 = document.querySelector(".section2");
+const section3 = document.querySelector(".section3");
+const section5 = document.querySelector(".section5");
 const carouselUL = document.querySelector(".carousel-track");
 const slides = Array.from(carouselUL.children);
 const carouselIndicator = document.querySelectorAll(".carousel-indicator")
 const carouselUpBtn = document.querySelector(".carousel-up-btn");
 const carouselDownBtn = document.querySelector(".carousel-down-btn");
+const sec2UpperRight = document.querySelector(".sec-2-upper-right");
+const sec2Btn = document.querySelector(".sec2Img2");
 const tl = gsap.timeline();
 const tl2 = gsap.timeline({paused: true});
 const tl3 = gsap.timeline();
+const tl4 = gsap.timeline({paused: true});
+const tl5 = gsap.timeline({paused: true});
+const tl6 = gsap.timeline({paused: true});
 
 let section2OffsetTop = 0;
 let section2OffsetHeight = 0;
 
-function offset(el) {
-    var rect = el.getBoundingClientRect(),
-    scrollLeft = window.pageXOffset || document.documentElement.scrollLeft,
-    scrollTop = window.pageYOffset || document.documentElement.scrollTop;
-    return { top: rect.top + scrollTop, left: rect.left + scrollLeft }
-}
-
-
-
+// function offset(el) {
+//     var rect = el.getBoundingClientRect(),
+//     scrollLeft = window.pageXOffset || document.documentElement.scrollLeft,
+//     scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+//     return { top: rect.top + scrollTop, left: rect.left + scrollLeft }
+// }
 
 burgerBtn.addEventListener("click",()=>{
     gsap.to("#phoneNavBar",{x:"-100%"})
@@ -31,7 +35,28 @@ burgerBtn.addEventListener("click",()=>{
     })
 })
 
-tl.fromTo(".video-bg",1,{height:"0%",width:"100%"},{height:"80%",ease: Power2})
+const userScroll = () =>{
+    mainContainer.removeAttribute("onscroll")
+    mainContainer.removeAttribute("onclick")
+    console.log("scrolled")
+    tl.play("skip")
+    setTimeout(()=>{
+        mainContainer.scrollTop = 0;
+        let imgHeight = slides[0].getBoundingClientRect().height;
+        slides.forEach((slide,index)=>{
+            slide.style.top = `${imgHeight*index}px`
+        })
+        section2OffsetTop = sec2UpperRight.offsetTop
+        section2OffsetHeight = sec2UpperRight.offsetHeight
+        section3OffsetTop = section3.offsetTop
+        section3OffsetHeight = section3.offsetHeight
+        section5OffsetTop = section5.offsetTop
+        section5OffsetHeight = section5.offsetHeight
+        console.log(section2OffsetHeight) 
+    },1500)
+}
+
+tl.fromTo(".video-bg",1,{height:"0%",width:"100%"},{height:"80%",ease:Power2})
   .to(".video-bg",1,{width:"80%",ease:Power2})
   .to(".slider",1,{x:"100%"},"-=1")
   .fromTo("#landing-logo",.5,{opacity:0,x:200},{opacity:1,x:0})
@@ -48,75 +73,142 @@ tl.addLabel("skip")
   .to(".slider2",{y:"-100%"})
   
 
-const userScroll = () =>{
-    mainContainer.removeAttribute("onscroll")
-    mainContainer.removeAttribute("onclick")
-    console.log("scrolled")
-    tl.play("skip")
-    setTimeout(()=>{
-        mainContainer.scrollTop = 0;
-        let imgHeight = slides[0].getBoundingClientRect().height;
-        slides.forEach((slide,index)=>{
-            slide.style.top = `${imgHeight*index}px`
-        })
-        section2OffsetTop = section2.offsetTop
-        section2OffsetHeight = section2.offsetHeight
-        
-        
-    },1500)
-}
 
-
-const progressTween = () =>{
-    const scrollPosition = (mainContainer.scrollTop + window.innerHeight);
-    const elPosition = (scrollPosition - 962);
-    const durationDistance = (window.innerHeight + 722);
-    const currentProgress = (elPosition / durationDistance);
-    tl3.progress(currentProgress)
-    
-}
 tl2.to(".sec2-heading-hide",.3,{top:0})
 .to(".hide1",.3,{top:0},"-=.1")
 .to(".hide2",.3,{top:0},"-=.1")
 .to(".hide3",.3,{top:0},"-=.1")
 .to(".hide4",.3,{top:0},"-=.1")
 .to("#sec-2-btn",{opacity:1},"-=.4")
-.fromTo(".sec2Img1",.5,{opacity:0,y:200},{opacity:1,y:0},"-=.6")
-.fromTo(".sec2Img2",.5,{opacity:0,y:-200},{opacity:1,y:0})
-.fromTo(".sec2Img3",.5,{opacity:0,y:200},{opacity:1,y:0})
-.fromTo(".sec2Img4",.5,{opacity:0,y:-200},{opacity:1,y:0});
+.fromTo(".sec2Img1",.5,{opacity:0,y:200},{opacity:1,y:0},"-=.8")
+.fromTo(".sec2Img2",.5,{opacity:0,y:-200},{opacity:1,y:0},"-=.8")
+.fromTo(".sec2Img3",.5,{opacity:0,y:200},{opacity:1,y:0},"-=.8")
+.fromTo(".sec2Img4",.5,{opacity:0,y:-200},{opacity:1,y:0},"-=.8")
 
-tl3.fromTo(".sec-2-track",{x:"100%", y:0},{x: "-100%",y:0});
+tl3.fromTo(".sec-2-track",{x:"0%", y:0},{x: "-83%",y:0});
+tl4.fromTo("#counter-container",1,{opacity:0,y:200},{opacity:1,y:0,ease:"back"})
+   .fromTo("#tag-line-1",1,{opacity:0,y:200},{opacity:1,y:0,ease:"back"},"-=.8");
 
-let options = {
-    root: null,
-    rootMargin: '0px 0px 0px 0px',
-    threshold: 0
-  };
+tl5.fromTo(".sec-3-img",1,{x:"0%"},{x:"-100%"})
+   .fromTo(".sec-3-review",1,{right:"-30%"},{right:"50%"},"-.01");
+tl6.fromTo(".sec-5-img",1,{x:"0%"},{x:"100%"})
 
-const dol = document.querySelector(".sec-2-upper");
-  
+const progressTween = () =>{
+    const scrollPosition = (mainContainer.scrollTop + 200);
+    const elPosition = (scrollPosition - section2OffsetTop);
+    const durationDistance = (window.innerHeight + section2OffsetHeight);
+    const currentProgress = (elPosition / durationDistance);
+    tl3.progress(currentProgress)
+    
+}
+const section3Progress = () =>{
+    const scrollPosition = (mainContainer.scrollTop + 300);
+    const elPosition = (scrollPosition - section3OffsetTop);
+    const durationDistance = (window.innerHeight + section3OffsetHeight);
+    const currentProgress = (elPosition / durationDistance);
+    tl5.progress(currentProgress)
+    
+}
+const section5Progress= () =>{
+    let scrollPosition = (mainContainer.scrollTop + 600);
+    const elPosition = (scrollPosition - section5OffsetTop);
+    const durationDistance = (window.innerHeight + section5OffsetHeight);
+    const currentProgress = (elPosition / durationDistance);
+    tl6.progress(currentProgress)
+    
+}
+
+
+//INTERSECTION OBSERVER
+const intersectionObserver = (callbacking,target) =>{
+    let options = {
+        root: null,
+        rootMargin: '0px 0px 0px 0px',
+        threshold: 0
+      };
+      let observer = new IntersectionObserver(callbacking, options);
+
+      observer.observe(target);
+}
+
+
+//CALLBACKS FOR INTERSECTION OBSERVER
+
 let callBack = (entries, observer) => {
     entries.forEach(entry =>{
         if (entry.intersectionRatio > 0){
-            gsap.ticker.add(progressTween);
             console.log("is intersecting")
             tl2.play();
-            dol.classList.add("fixed")
-        }else{
-            console.log("not intersecting");
-            gsap.ticker.remove(progressTween);
-            tl2.reverse();
+            
         }
     });
 };
-let observer = new IntersectionObserver(callBack, options);
+const dol = document.querySelector(".sec-2-upper");
+let callBack2 = (entries,observer)=>{
+    entries.forEach(entry =>{
+        if (entry.intersectionRatio > 0){
+            gsap.ticker.add(progressTween);
+            dol.classList.add("fixed");
+            console.log("button seen");
+        }else{
+            gsap.ticker.remove(progressTween);
+            tl3.reverse();
+        }
+    });
+}
 
-observer.observe(section2);
+//for counter
+const counter = document.getElementById("counter");
+const c = new CountUp("counter", 0,753)
+const callBack3 = (entries,observer) =>{
+    entries.forEach(entry=>{
+        if (entry.intersectionRatio > 0){
+            tl4.play();
+            c.start();
+        }
+    })
+}
+
+let section3ImageCallback = (entries,observer)=>{
+    entries.forEach(entry =>{
+        if (entry.intersectionRatio > 0){
+            gsap.ticker.add(section3Progress);
+        }else{
+            gsap.ticker.remove(section3Progress);
+            tl5.reverse();
+        }
+    });
+}
+let section5ImageCallback = (entries,observer)=>{
+    entries.forEach(entry =>{
+        if (entry.intersectionRatio > 0){
+            gsap.ticker.add(section5Progress);
+        }else{
+            gsap.ticker.remove(section5Progress);
+            // tl5.reverse();
+        }
+    });
+}
+
+
+intersectionObserver(callBack,section2)
+intersectionObserver(callBack2,sec2UpperRight);
+intersectionObserver(callBack3,counter);
+intersectionObserver(section3ImageCallback,section3);
+intersectionObserver(section5ImageCallback,section5);
 
 
 
-  
+//scroll progressbar
+const scrollProgress = () =>{
+    let winScroll = mainContainer.scrollTop;
+    let height = mainContainer.scrollHeight - mainContainer.clientHeight;   
+    var scrolled = (winScroll / height) * 100;
+    document.getElementById("myBar").style.height = `${scrolled}%`
+}
+mainContainer.onscroll = ()=>{
+    scrollProgress();
+}
 
 
 
@@ -175,6 +267,7 @@ carouselUpBtn.addEventListener("click",e=>{
     carouselIndicator[activeIndicator-1].classList.add("current-slide-indicator")
     activeIndicator--;
 })
+
 
 
 
